52fad8ba4bc1a74d74011c7c42aff28277f09712
Jose <jose.maureira.b@usach.cl>
2020-07-31 00:12:31 -0400

    Adv: Charge images 100% functional

--- pages/users/_name.vue
+++ pages/users/_name.vue
@@ -172,7 +172,7 @@ export default {
       const formData = new FormData()
       formData.append('user', self.user.name)
       Array.from(Array(fileList.length).keys()).map((x) => {
-        formData.append(fieldName, fileList[x], fileList[x].name)
+        formData.append(fieldName, fileList[x], this.user.name)
       })
       return self.$store.dispatch('user/upload', formData)
         .then((result) => {
