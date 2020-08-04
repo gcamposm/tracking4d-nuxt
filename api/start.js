bdc430c4ac673f6e58c54b8111e093cf72284bac
Guillermo Campos <guillermo.campos@usach.cl>
2020-08-04 16:32:06 -0400

    Fix: Cerbot on express

--- api/start.js
+++ api/start.js
@@ -13,3 +13,21 @@ consola.ready({
   message: `API listening on http://${host}:${port}`,
   badge: true
 })
+
+
+const fs = require('fs')
+const https = require('https')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello HTTPS!')
+})
+
+
+https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/tracking4dd.tk/key.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/tracking4dd.tk/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/tracking4dd.tk/chain.pem')
+}, app).listen(3000, () => {
+  console.log('Listening...')
+})
