a62ad952aea6bee4e456715619720835183a48af
Jose <jose.maureira.b@usach.cl>
2020-09-11 12:32:30 -0300

    Fix: Delete temp in normal recognize

--- store/face.js
+++ store/face.js
@@ -180,6 +180,60 @@ export const actions = {
       faceapi.draw.drawFaceLandmarks(canvasDiv, detection.landmarks, { lineWidth: state.landmarks.lineWidth, drawLines: state.landmarks.drawLines })
     }
   },
+  drawWithoutTemp({
+    commit,
+    state
+  }, {
+    canvasDiv,
+    canvasCtx,
+    detection,
+    options
+  }) {
+    let emotions = ''
+    // filter only emontions above confidence treshold and exclude 'neutral'
+    if (options.expressionsEnabled && detection.expressions) {
+      for (const expr in detection.expressions) {
+        if (detection.expressions[expr] > state.expressions.minConfidence && expr !== 'neutral') {
+          emotions += ` ${expr} &`
+        }
+      }
+      if (emotions.length) {
+        emotions = emotions.substring(0, emotions.length - 2)
+      }
+    }
+    let name = ''
+    if (options.descriptorsEnabled && detection.recognition) {
+      name = detection.recognition.toString(state.descriptors.withDistance)
+    }
+    const text = `${name}${emotions ? (name ? ' is ' : '') : ''}${emotions}`
+    const box = detection.box || detection.detection.box
+    this.state.finalEmotion = emotions
+    if (options.detectionsEnabled && box) {
+      // draw box
+      canvasCtx.strokeStyle = state.detections.boxColor
+      canvasCtx.lineWidth = state.detections.lineWidth
+      canvasCtx.strokeRect(box.x, box.y, box.width, box.height)
+    }
+    if (text && detection && box) {
+      // draw text
+      var numTemp = (Math.random() * 2) + 35.6
+      var s = numTemp.toString()
+      var decimalLength = s.indexOf('.') + 1
+      var numStr = s.substr(0, decimalLength + 2)
+      numTemp = Number(numStr)
+      const padText = 2 + state.detections.lineWidth
+      canvasCtx.fillStyle = state.detections.textColor
+      canvasCtx.font = state.detections.fontSize + 'px ' + state.detections.fontStyle
+      canvasCtx.fillText(text, box.x + padText, box.y + box.height + padText + (state.detections.fontSize * 0.6))
+    }
+
+    if (options.landmarksEnabled && detection.landmarks) {
+      faceapi.draw.drawFaceLandmarks(canvasDiv, detection.landmarks, {
+        lineWidth: state.landmarks.lineWidth,
+        drawLines: state.landmarks.drawLines
+      })
+    }
+  },
 
   async createCanvas ({ commit, state }, elementId) {
     const canvas = await faceapi.createCanvasFromMedia(document.getElementById(elementId))
